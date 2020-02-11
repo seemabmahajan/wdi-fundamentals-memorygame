98221de0c5eb918e0c5a128b5a3c8b18c9ca0a0f
SeemaMahajan <seemamahajan@Seemas-MacBook-Pro.local>
2020-02-10 22:41:33 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,25 +1,51 @@
 
-let cards = ["queen","queen","king","king"];
+let cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank:  "queen",
+		suit:  "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage:  "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit:  "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+
+
+];
 let cardsInPlay = [];
 
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match! ");
+		alert("You found a match! ");
 	}
 	else{
-		console.log("Sorry, Try Again. ");
-	}
+		alert("Sorry, Try Again. ");
+	};
 }
 
 function flipCard(cardId){
-	cardsInPlay.push(cards[cardId]);
-	console.log("User Pushed : " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User Pushed : " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	
 	if(cardsInPlay.length === 2){
 	checkForMatch();
+	cardsInPlay = [];
     }
 }
 
-flipCard(0);
 flipCard(1);
+flipCard(3);
 
